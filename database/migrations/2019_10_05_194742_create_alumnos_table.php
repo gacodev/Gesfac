068bbac4b08f6dea82272dcf5c8eb6f0b714412b
sarertnoc <g.sarertnoc12@gmail.com>
2019-11-18 11:45:22 -0500

    cambios vista de alumnos para registro

--- database/migrations/2019_10_05_194742_create_alumnos_table.php
+++ database/migrations/2019_10_05_194742_create_alumnos_table.php
@@ -19,14 +19,23 @@ class CreateAlumnosTable extends Migration
             $table->string('telefono');
             $table->string('direccion');
             $table->string('correo');
-            $table->string('numero_documento');
-            $table->string('excusado');
+            $table->string('numero_documento')->nullable();
+            $table->string('rh')->nullable();
+            $table->string('numero_documento_padre')->nullable();
+            $table->string('nombre_padre')->nullable();
+            $table->string('telefono_padre')->nullable();
+            $table->string('ocupacion_padre')->nullable();
+            $table->string('direccion_padre')->nullable();
+            $table->string('numero_documento_madre')->nullable();
+            $table->string('nombre_madre')->nullable();
+            $table->string('telefono_madre')->nullable();
+            $table->string('ocupacion_madre')->nullable();
+            $table->string('direccion_madre')->nullable();
+            $table->string('excusado')->nullable();
             $table->unsignedBigInteger('escuadron');
             $table->foreign('escuadron')->references('id')->on('escuadrones');
-
             $table->unsignedBigInteger('tipo_documento');
             $table->foreign('tipo_documento')->references('id')->on('tipo_documento');
-
             $table->unsignedBigInteger('novedad')->nullable();
             $table->foreign('novedad')->references('id')->on('novedades');
 
