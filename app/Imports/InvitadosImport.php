7f93208d6b581560bfdc2ce0d7c7ef2c04ecd159
sarertnoc <g.sarertnoc12@gmail.com>
2019-11-19 15:18:14 -0500

    trabajo en importacion falta la relacion para poder traer los datos y hacer la asociacion

--- app/Imports/InvitadosImport.php
+++ app/Imports/InvitadosImport.php
@@ -3,6 +3,8 @@
 namespace App\Imports;
 use App\visitante;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Facades\DB;
+
 
 class InvitadosImport implements ToModel
 {
@@ -13,11 +15,16 @@ class InvitadosImport implements ToModel
     */
     public function model(array $row)
     {
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         return new visitante([
+            'tipo_documento'=>    $row[0],
             'numero_documento' => $row[1],
-            'nombre' => $row[2],
-            'telefono' => $row[3],
+            'nombre' =>           $row[2],
+            'telefono' =>           $row[3],
+
 
         ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
