e6139f54e65fcba9f9bb8a4c5c4ee4a1bff7d144
rjcerquera <jacerquera021@misena.edu.co>
2019-12-17 05:24:17 -0500

    agregando sanidad a los alumnos

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Alumno;
 use App\Escuadron;
 use App\Imports\AlumnosImport;
 use App\Imports\ArmerilloImport;
 use App\Imports\InvitadosImport;
 
 
+use App\Sanidad;
 use App\TipoDocumento;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Http\Request;
@@ -26,6 +28,22 @@ class ReportesController extends Controller
 
         $import = new InvitadosImport();
         $import->import($file);
+
+        $alumnos = Alumno::all();
+
+        $data = [
+            ['tipo_familiar' => 'PADRE'],
+            ['tipo_familiar' => 'MADRE'],
+        ];
+
+        foreach ($alumnos as $alumno) {
+
+            if(!Sanidad::where("alumno", $alumno->id)->count()){
+                Sanidad::create([
+                   "alumno" => $alumno->id
+                ]);
+            }
+        }
 
         return redirect()->route('registro_inv')->with('failures', $import->failures());
     }
